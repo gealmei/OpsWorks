bc58ca44dbceeebde28c92df8b85b7890e5a4baa
Guilherme Elias Leite de Almeida <you@example.com>
2017-04-07 16:05:26 -0300

    first cookbooks

---
+++ elasticsearch/metadata.rb
@@ -0,0 +1,7 @@
+name 'elasticsearch'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures elasticsearch'
+long_description 'Installs/Configures elasticsearch'
+version '0.1.0'
---
+++ kibana/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: kibana
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
